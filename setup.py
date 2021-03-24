bb68bb44aa918e667be61894bfdc24ad7d71324a
Kyostenas <yaelb.ramirez@alumno.udg.mx>
2021-03-24 13:18:01 -0600

    modified setup

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@ from distutils.core import setup
 setup(
   name = 'prettyTables',         
   packages = ['prettyTables'],   
-  version = '0.1',      
+  version = '0.2',      
   license='MIT',        
   description = 'All sorts of tables and graphics for console (for now just tables) ',   
   author = 'Benjamin Ramirez',                   
   author_email = 'chilerito12@gmail.com',      
-  url = 'https://github.com/Kyostenas/prettyGraphics',   
-  download_url = 'https://github.com/Kyostenas/prettyTables/archive/v0.1-beta.1.tar.gz',    
+  url = 'https://github.com/Kyostenas/prettyTables',   
+  download_url = 'https://github.com/Kyostenas/prettyTables/archive/v0.2-beta.1.tar.gz',    
   keywords = ['console', 'graphics'],   
   install_requires=[ 
       'nltk'
       ],
   classifiers=[
-    'Development Status :: Beta',      
+    'Development Status :: 4 - Beta',      
     'Intended Audience :: Developers',      
-    'Topic :: Software Development :: Console tools',
+    'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   
     'Programming Language :: Python :: 3',      
     'Programming Language :: Python :: 3.4',
