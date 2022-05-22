b8513255c94171435677c582fecb5382d8de592c
Kyostenas <yaelb.ramirez@alumno.udg.mx>
2022-05-22 07:32:54 -0500

    docs: finished checking options.py

--- prettyTables/options.py
+++ prettyTables/options.py
@@ -2,21 +2,46 @@
 OPTIONS
 
 some parameters for the creation of styles,
-formatting, and other options
+formatting, and other things.
 """
 
 import re
 
 
+# +-------------------------+ CONSTANTS +------------------------+
+
+# For styles without vertical lines.
 INVISIBLE_SEPARATOR = ' '
-CELL_MARGIN = 1
-ESCAPE_CODES = ['\n', '\r']
-MIN_COLUMN_WIDTH = 2
+
+# Default value for an empty cell.
 NONE_VALUE_REPLACEMENT = ''
+
+# Default value to fill cells when aligning.
 DEFAULT_FILL_CHAR = ' '
+
+# The style is set if the user doesn't choose or puts an incorrect value.
 DEFAULT_STYLE = 'grid_eheader'
-I_COL_TIT = 'i'  # INDEX COLUMN TITLE
+
+# Default index column header.
+I_COL_TIT = 'i'
+
+# Default space a cell has on each side (after aligning).
+CELL_MARGIN = 1
+
+# Not used yet.
 DEFAULT_TABLE_ALIGNMENT = 'l'
-INT_FILTER = re.compile(r'^[-]?[0-9]*$').match
-FLT_FILTER = re.compile(r'^[-]?[0-9]*[.][0-9]*$').match
+
+# Default string that is shown when a cell is trimmed.
 DEFAULT_TRIMMING_SIGN = '...'
+
+
+# +--------------------------+ REGEX +---------------------------+
+
+# To find integer numbers.
+INT_FILTER = re.compile(r'^[-]?[0-9]*$').match
+
+# TO find float numbers (not exponential ones)
+FLT_FILTER = re.compile(r'^[-]?[0-9]*[.][0-9]*$').match
+
+
+# +--------------------------------------------------------------+
